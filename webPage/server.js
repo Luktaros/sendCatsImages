112ade0d3cd675f563b87d2b8bd71e8db8843c9d
Lucio Perez <lucioperez.student@gmail.com>
2023-09-17 20:44:10 -0300

    WIP

--- webPage/server.js
+++ webPage/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+// import axios from 'axios';
 
 const app = express();
 const port = 3000;
@@ -7,12 +8,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
-app.post('/submit', (req, res) =>{
+app.post('/submit', async (req, res) =>{
   // Execute GCloud Function with request data
+  console.log(req.body);
+  // const gcfUrl = 'https://us-central1-luk-firebase-testing.cloudfunctions.net/sendCuteCatsViaEmail';
+  // await axios.post(gcfUrl, req.body);
+
   res.send('Email with cat images send!');
 })
+
+
 
 app.listen(port, () =>{
   console.log('Webpage server is running.');
 });
-
